6b0ac40efa93bb05b34c6e1340fc1dbe0dceb2b7
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-18 12:26:50 +0530

    Update 2-Arranging_cats.cpp

--- Round-920(Div. 3)/2-Arranging_cats.cpp
+++ Round-920(Div. 3)/2-Arranging_cats.cpp
@@ -1,55 +1,38 @@
 #include <bits/stdc++.h>
+
+#define itn int
+#define all(x) (x).begin(), (x).end()
+#define make_unique(x) sort(all((x))); (x).resize(unique(all((x))) - (x).begin())
+
 using namespace std;
 
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        
-    }
-    return 0;
+inline int nxt() {
+	int x;
+	cin >> x;
+	return x;
 }
 
+void solve() {
+	int n = nxt();
+	string s, t;
+	cin >> s >> t;
+	int c[2] = {0, 0};
+	for (int i = 0; i < n; ++i) {
+		if (s[i] != t[i]) {
+			c[s[i] - '0'] += 1;
+		}
+	}
+	cout << max(c[0], c[1]) << "\n";
+}
 
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 
+	int t = nxt();
+	while (t--) {
+		solve();
+	}
 
-// #include <bits/stdc++.h>
-
-// #define itn int
-// #define all(x) (x).begin(), (x).end()
-// #define make_unique(x) sort(all((x))); (x).resize(unique(all((x))) - (x).begin())
-
-// using namespace std;
-
-// inline int nxt() {
-// 	int x;
-// 	cin >> x;
-// 	return x;
-// }
-
-// void solve() {
-// 	int n = nxt();
-// 	string s, t;
-// 	cin >> s >> t;
-// 	int c[2] = {0, 0};
-// 	for (int i = 0; i < n; ++i) {
-// 		if (s[i] != t[i]) {
-// 			c[s[i] - '0'] += 1;
-// 		}
-// 	}
-// 	cout << max(c[0], c[1]) << "\n";
-// }
-
-// int main() {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(nullptr);
-
-// 	int t = nxt();
-// 	while (t--) {
-// 		solve();
-// 	}
-
-// 	return 0;
-// }
+	return 0;
+}
